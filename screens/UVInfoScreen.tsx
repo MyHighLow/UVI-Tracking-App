0fd5abde77054c48e210ac32e2243bb8adab1f29
Aaron Glidden <aglidden9@gmail.com>
2024-02-26 09:40:12 -0600

    filler text for UV info screen

--- screens/UVInfoScreen.tsx
+++ screens/UVInfoScreen.tsx
@@ -1,5 +1,6 @@
 import {
     SafeAreaView,
+    ScrollView,
     StyleSheet,
     Text,
     TouchableOpacity,
@@ -12,7 +13,29 @@ import Colors from "../constants/colors";
 export default function UVInfoScreen() {
     return (
         <SafeAreaView style={styles.container}>
-            <View ></View>
+            <ScrollView style={styles.scrollView}>
+                <Text style={styles.sectionText}>What is UV Index?</Text>
+                <Text style={styles.paragraph}>
+                    The UV Index is a measure of the strength of the sun's ultraviolet (UV) rays. It is a
+                    scale primarily used in daily forecasts aimed at the general public. The UV Index scale
+                    was developed by Canadian scientists in 1992, and then adopted and standardized by the
+                    United Nations World Health Organization and World Meteorological Organization in 1994.
+                </Text>
+                <Text style={styles.paragraph}>
+                    The UV Index is a useful tool for sun protection and is increasingly used to inform the public
+                    about the daily UV levels. It is also used to inform the public about the need to use sun
+                    protection and sunburn prevention measures, such as sunscreen, hats, protective clothing,
+                    sunglasses, and shade.
+                </Text>
+                <Text style={styles.paragraph}>
+                    The UV Index is a linear scale, with higher values representing a greater risk of harm from
+                    unprotected sun exposure. A UV Index reading of 0 to 2 means low danger from the sun's UV rays
+                    for the average person. A UV Index reading of 3 to 5 means moderate risk of harm from unprotected
+                    sun exposure. A UV Index reading of 6 to 7 means high risk of harm from unprotected sun exposure.
+                    A UV Index reading of 8 to 10 means very high risk of harm from unprotected sun exposure. A UV
+                    Index reading of 11 or more means extreme risk of harm from unprotected sun exposure.
+                </Text>
+            </ScrollView>
         </SafeAreaView>
     );
 }
@@ -23,5 +46,17 @@ const styles = StyleSheet.create({
         backgroundColor: "white",
         alignItems: "center",
         justifyContent: "center",
+    },
+    scrollView: {
+        backgroundColor: "white",
+    },
+    sectionText: {
+        fontSize: 24,
+        fontWeight: "bold",
+        margin: 20,
+    },
+    paragraph: {
+        fontSize: 16,
+        margin: 20,
     },
 });
