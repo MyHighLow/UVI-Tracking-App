be329fd34a3f7c9ce3484586e04ebfb9f12590fd
Milo <>
2024-04-23 19:01:57 -0500

    Display empty legends

--- components/BarItem.tsx
+++ components/BarItem.tsx
@@ -6,16 +6,26 @@ export type Props = {
   barPercentage: string;
 };
 
+let generateStyle = function(props : Props) {
+  let ret = {
+    flex:1,
+    height: props.barPercentage,
+    backgroundColor: "#F53",
+    borderWidth: 1,
+    borderColor: "white",
+    borderStyle:"solid",
+    justifyContent: "flex-end",
+  };
+
+  if (props.barPercentage == "0%") {
+    ret["height"] = "100%";
+    ret["backgroundColor"] = "white";
+  }
+  return ret;
+};
+
 export default function BarItem(props : Props) {
-  return <View style={{
-      flex:1,
-      height: props.barPercentage,
-      backgroundColor: "#F53",
-      borderWidth: 1,
-      borderColor: "white",
-      borderStyle:"solid",
-      justifyContent: "flex-end",
-    }}>
+  return <View style={generateStyle(props)}>
     <Text style={styles.legend}>{props.legend}</Text>
   </View>;
 };
